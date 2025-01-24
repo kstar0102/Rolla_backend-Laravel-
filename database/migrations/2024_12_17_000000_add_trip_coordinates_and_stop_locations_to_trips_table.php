4be86cf92257d629bfe3fe809fbde62f863e459b
Senior Mobile app | Web | API Developer <seniordev52@gmail.com>
2025-01-24 11:32:42 +0300

    fix trips, add new column

--- database/migrations/2024_12_17_000000_add_trip_coordinates_and_stop_locations_to_trips_table.php
+++ database/migrations/2024_12_17_000000_add_trip_coordinates_and_stop_locations_to_trips_table.php
@@ -9,16 +9,36 @@ return new class extends Migration
     public function up()
     {
         Schema::table('trips', function (Blueprint $table) {
-            $table->json('trip_coordinates')->nullable()->change();
-            $table->json('stop_locations')->nullable()->change();
+            if (Schema::hasColumn('trips', 'trip_coordinates')) {
+                $table->json('trip_coordinates')->nullable()->change();
+            } else {
+                $table->json('trip_coordinates')->nullable();
+            }
+
+            if (Schema::hasColumn('trips', 'stop_locations')) {
+                $table->json('stop_locations')->nullable()->change();
+            } else {
+                $table->json('stop_locations')->nullable();
+            }
+
+            if (Schema::hasColumn('trips', 'destination_text_address')) {
+                $table->json('destination_text_address')->nullable()->change();
+            } else {
+                $table->json('destination_text_address')->nullable();
+            }
         });
     }
 
     public function down()
     {
         Schema::table('trips', function (Blueprint $table) {
-            $table->text('trip_coordinates')->nullable()->change();
-            $table->text('stop_locations')->nullable()->change();
+            if (Schema::hasColumn('trips', 'trip_coordinates')) {
+                $table->text('trip_coordinates')->nullable()->change();
+            }
+
+            if (Schema::hasColumn('trips', 'stop_locations')) {
+                $table->text('stop_locations')->nullable()->change();
+            }
         });
     }
 };
